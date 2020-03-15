1819a77819aa3449e1df189753806228cb32e9be
Elijah Ozhmegov <ElijahOzhmegov@gmail.com>
2020-03-15 17:24:53 +0100

    add substring solution

--- LeetCode/Longest Substring Without Repeating Characters.py
+++ LeetCode/Longest Substring Without Repeating Characters.py
@@ -1,25 +1,33 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        faced_characters = {}
         max_max = 0
-        max_len = 0
-        faced_characters = []
 
-        for character in s:
-            if character not in faced_characters:
-                max_len += 1
-                faced_characters.append(character)
+        n = len(s)
+        for i in range(n):
+            if s[i] in faced_characters:
+                max_max = len(faced_characters) if len(faced_characters) > max_max else max_max
+                marker = faced_characters[s[i]]
+                for faced_character in sorted(faced_characters.items(), key=lambda kv: kv[1]):
+                    if faced_character[1] <= marker:
+                        faced_characters.pop(faced_character[0])
+                    else:
+                        break
+
+                faced_characters[s[i]] = i
             else:
-                faced_characters = [character]
-                max_max = max_len if max_len > max_max else max_max
-                max_len = 1
+                faced_characters[s[i]] = i
 
-        return max_len if max_len > max_max else max_max
+        return len(faced_characters) if len(faced_characters) > max_max else max_max
+
 
 
 if __name__ == '__main__':
     ss = Solution()
-    # assert ss.lengthOfLongestSubstring('abcabcbb') == 3
-    # assert ss.lengthOfLongestSubstring('bbbbb') == 1
-    # assert ss.lengthOfLongestSubstring('pwwkew') == 3
-    # assert ss.lengthOfLongestSubstring(' ') == 1
+    assert ss.lengthOfLongestSubstring('cdd') == 2
+    assert ss.lengthOfLongestSubstring('abcabcbb') == 3
+    assert ss.lengthOfLongestSubstring('bbbbb') == 1
+    assert ss.lengthOfLongestSubstring('pwwkew') == 3
+    assert ss.lengthOfLongestSubstring(' ') == 1
     assert ss.lengthOfLongestSubstring('dvdf') == 3
+    assert ss.lengthOfLongestSubstring('bbtablud') == 6
